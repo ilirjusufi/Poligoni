51eb70621402684c60326375b77b48cfe6f85845
HAjrAhmeti <hajreahmeti@gmail.com>
2020-04-28 12:38:13 +0200

    Login GUI (Done)

--- Poligoni/frmLogin.Designer.cs
+++ Poligoni/frmLogin.Designer.cs
@@ -28,68 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtUser = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
-            this.btnKyqu = new System.Windows.Forms.Button();
-            this.btnAnulo = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // txtUser
-            // 
-            this.txtUser.Location = new System.Drawing.Point(65, 77);
-            this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(191, 20);
-            this.txtUser.TabIndex = 0;
-            // 
-            // txtPass
-            // 
-            this.txtPass.Location = new System.Drawing.Point(65, 115);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(191, 20);
-            this.txtPass.TabIndex = 0;
-            // 
-            // btnKyqu
-            // 
-            this.btnKyqu.Location = new System.Drawing.Point(65, 159);
-            this.btnKyqu.Name = "btnKyqu";
-            this.btnKyqu.Size = new System.Drawing.Size(75, 23);
-            this.btnKyqu.TabIndex = 1;
-            this.btnKyqu.Text = "Kyqu";
-            this.btnKyqu.UseVisualStyleBackColor = true;
-            this.btnKyqu.Click += new System.EventHandler(this.btnKyqu_Click);
-            // 
-            // btnAnulo
-            // 
-            this.btnAnulo.Location = new System.Drawing.Point(169, 159);
-            this.btnAnulo.Name = "btnAnulo";
-            this.btnAnulo.Size = new System.Drawing.Size(75, 23);
-            this.btnAnulo.TabIndex = 1;
-            this.btnAnulo.Text = "Anulo";
-            this.btnAnulo.UseVisualStyleBackColor = true;
-            // 
-            // frmLogin
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 331);
-            this.Controls.Add(this.btnAnulo);
-            this.Controls.Add(this.btnKyqu);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.txtUser);
-            this.Name = "frmLogin";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = " ";
-            this.Load += new System.EventHandler(this.frmLogin_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.txtUser = new System.Windows.Forms.TextBox();
+			this.btnAnulo = new System.Windows.Forms.Button();
+			this.txtPass = new System.Windows.Forms.TextBox();
+			this.btnKyqu = new System.Windows.Forms.Button();
+			this.jGradientPanel1 = new JGradient_Panel.JGradientPanel();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.jGradientPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(72, 255);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(28, 31);
+			this.pictureBox3.TabIndex = 11;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(72, 209);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(28, 31);
+			this.pictureBox2.TabIndex = 10;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(115, 30);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(139, 138);
+			this.pictureBox1.TabIndex = 9;
+			this.pictureBox1.TabStop = false;
+			// 
+			// txtUser
+			// 
+			this.txtUser.BackColor = System.Drawing.Color.SeaGreen;
+			this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtUser.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUser.ForeColor = System.Drawing.Color.White;
+			this.txtUser.Location = new System.Drawing.Point(100, 209);
+			this.txtUser.Multiline = true;
+			this.txtUser.Name = "txtUser";
+			this.txtUser.Size = new System.Drawing.Size(199, 31);
+			this.txtUser.TabIndex = 5;
+			this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// btnAnulo
+			// 
+			this.btnAnulo.BackColor = System.Drawing.Color.SeaGreen;
+			this.btnAnulo.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.btnAnulo.FlatAppearance.BorderSize = 2;
+			this.btnAnulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAnulo.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAnulo.ForeColor = System.Drawing.Color.White;
+			this.btnAnulo.Location = new System.Drawing.Point(216, 313);
+			this.btnAnulo.Name = "btnAnulo";
+			this.btnAnulo.Size = new System.Drawing.Size(98, 42);
+			this.btnAnulo.TabIndex = 7;
+			this.btnAnulo.Text = "Anulo";
+			this.btnAnulo.UseVisualStyleBackColor = false;
+			// 
+			// txtPass
+			// 
+			this.txtPass.BackColor = System.Drawing.Color.SeaGreen;
+			this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtPass.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPass.ForeColor = System.Drawing.Color.White;
+			this.txtPass.Location = new System.Drawing.Point(100, 257);
+			this.txtPass.Multiline = true;
+			this.txtPass.Name = "txtPass";
+			this.txtPass.PasswordChar = '#';
+			this.txtPass.Size = new System.Drawing.Size(199, 29);
+			this.txtPass.TabIndex = 6;
+			this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// btnKyqu
+			// 
+			this.btnKyqu.BackColor = System.Drawing.Color.SeaGreen;
+			this.btnKyqu.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.btnKyqu.FlatAppearance.BorderSize = 2;
+			this.btnKyqu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnKyqu.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnKyqu.ForeColor = System.Drawing.Color.White;
+			this.btnKyqu.Location = new System.Drawing.Point(53, 313);
+			this.btnKyqu.Name = "btnKyqu";
+			this.btnKyqu.Size = new System.Drawing.Size(98, 42);
+			this.btnKyqu.TabIndex = 8;
+			this.btnKyqu.Text = "Kyqu";
+			this.btnKyqu.UseVisualStyleBackColor = false;
+			// 
+			// jGradientPanel1
+			// 
+			this.jGradientPanel1.ColorBottom = System.Drawing.Color.MediumSeaGreen;
+			this.jGradientPanel1.ColorTop = System.Drawing.Color.SeaGreen;
+			this.jGradientPanel1.Controls.Add(this.pictureBox4);
+			this.jGradientPanel1.Controls.Add(this.pictureBox5);
+			this.jGradientPanel1.Controls.Add(this.pictureBox6);
+			this.jGradientPanel1.Controls.Add(this.textBox1);
+			this.jGradientPanel1.Controls.Add(this.button1);
+			this.jGradientPanel1.Controls.Add(this.textBox2);
+			this.jGradientPanel1.Controls.Add(this.button2);
+			this.jGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.jGradientPanel1.Location = new System.Drawing.Point(0, 0);
+			this.jGradientPanel1.Name = "jGradientPanel1";
+			this.jGradientPanel1.Size = new System.Drawing.Size(404, 400);
+			this.jGradientPanel1.TabIndex = 12;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(123, 315);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(28, 31);
+			this.pictureBox4.TabIndex = 4;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(123, 269);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(28, 31);
+			this.pictureBox5.TabIndex = 3;
+			this.pictureBox5.TabStop = false;
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+			this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+			this.pictureBox6.Location = new System.Drawing.Point(166, 90);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(139, 138);
+			this.pictureBox6.TabIndex = 2;
+			this.pictureBox6.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.SeaGreen;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(151, 269);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(199, 31);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.SeaGreen;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button1.FlatAppearance.BorderSize = 2;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(267, 373);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(98, 42);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Anulo";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.SeaGreen;
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox2.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.ForeColor = System.Drawing.Color.White;
+			this.textBox2.Location = new System.Drawing.Point(151, 317);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.PasswordChar = '#';
+			this.textBox2.Size = new System.Drawing.Size(199, 29);
+			this.textBox2.TabIndex = 0;
+			this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.SeaGreen;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button2.FlatAppearance.BorderSize = 2;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(104, 373);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(98, 42);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Kyqu";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// frmLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(404, 400);
+			this.Controls.Add(this.jGradientPanel1);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.txtUser);
+			this.Controls.Add(this.btnAnulo);
+			this.Controls.Add(this.txtPass);
+			this.Controls.Add(this.btnKyqu);
+			this.Name = "frmLogin";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = " ";
+			this.Load += new System.EventHandler(this.frmLogin_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.jGradientPanel1.ResumeLayout(false);
+			this.jGradientPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnAnulo;
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.Button btnKyqu;
-        private System.Windows.Forms.Button btnAnulo;
+        private JGradient_Panel.JGradientPanel jGradientPanel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
